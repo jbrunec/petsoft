b6f69e19391064b5424274a480adc63d8316990d
Jernej Brunec <jernej.brunec@gmail.com>
2024-09-10 09:30:51 +0200

    Add login & sgnup pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,20 @@
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
 import React from "react";
 
 export default function Login() {
-  return <div>Login page</div>;
+  return (
+    <main>
+      <H1 className="text-center mb-5">Log In</H1>
+
+      <AuthForm />
+      <p className="mt-6 text-zinc-500 text-sm">
+        Don&apos;t have an account?{" "}
+        <Link href="/signup" className="font-medium">
+          Sign up
+        </Link>
+      </p>
+    </main>
+  );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,20 @@
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
 import React from "react";
 
-export default function SingUp() {
-  return <div>SingUp</div>;
+export default function Login() {
+  return (
+    <main>
+      <H1 className="text-center mb-5">Sign Up</H1>
+
+      <AuthForm />
+      <p className="mt-6 text-zinc-500 text-sm">
+        Already have an account?{" "}
+        <Link href="/login" className="font-medium">
+          Log in
+        </Link>
+      </p>
+    </main>
+  );
 }
